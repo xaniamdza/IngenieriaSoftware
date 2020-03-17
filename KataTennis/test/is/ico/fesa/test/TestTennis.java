df1b704daf763b2fac93c50b4a512109505a7e04
Xania Mendoza <31690927+xaniamdza@users.noreply.github.com>
2020-03-16 21:46:20 -0600

    Add files via upload

---
+++ KataTennis/test/is/ico/fesa/test/TestTennis.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is.ico.fesa.test;
+
+import is.ico.fesa.tennis.Tennis;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Xania Mendoza
+ */
+public class TestTennis {
+    
+    private Tennis objeto;
+    public TestTennis() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        objeto = new Tennis();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    @Test
+    public void ganadorFinal(){
+        objeto.setVictoria1(1);
+        objeto.setVictoria2(0);
+        int esperado = 1;
+        assertEquals(esperado, objeto.ganadorFinal());
+    }
+}
